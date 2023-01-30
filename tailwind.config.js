4617d5f3ab3169e1e55394db70a5df6008617ba7
Lucas Nhimi <lucasnhimi@gmail.com>
2023-01-29 21:34:11 -0300

    Initial code

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        secondary: 'var(--color-secondary)',
+        primary: {
+          DEFAULT: 'var(--color-primary)',
+          100: 'var(--color-primary-100)',
+          200: 'var(--color-primary-200)',
+          300: 'var(--color-primary-300)',
+          400: 'var(--color-primary-400)',
+          500: 'var(--color-primary-500)',
+          600: 'var(--color-primary-600)',
+          700: 'var(--color-primary-700)',
+          800: 'var(--color-primary-800)',
+          900: 'var(--color-primary-900)',
+        },
+        gray: {
+          700: '#40414F',
+          800: '#343541',
+          900: '#202123',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
