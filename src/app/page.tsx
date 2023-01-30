dfa6d4c7d69702079e117f6b7f627bf32b0fe486
Lucas Nhimi <lucasnhimi@gmail.com>
2023-01-30 17:16:55 -0300

    Loading message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -203,7 +203,6 @@ export default function Home() {
         prompt: requestMessage,
       }),
     });
-    console.log('Edge function returned.');
 
     if (!response.ok) {
       throw new Error(response.statusText);
@@ -227,8 +226,9 @@ export default function Home() {
     }
 
     setLoading(false);
-    console.log(responseMessage);
   };
+
+  console.log('loading', loading);
 
   return (
     <main className='relative h-full w-full transition-width flex flex-col overflow-hidden items-stretch flex-1'>
@@ -236,7 +236,13 @@ export default function Home() {
         <div className='h-full dark:bg-gray-800'>
           <div className='flex flex-col items-center text-sm h-full dark:bg-gray-800'>
             {responseMessage === '' ? (
-              <Hero />
+              loading ? (
+                <div className='flex flex-col items-center justify-center h-full text-gray-100 font-bold'>
+                  Carregando...
+                </div>
+              ) : (
+                <Hero />
+              )
             ) : (
               <Chat
                 requestMessage={requestMessage}
@@ -247,39 +253,43 @@ export default function Home() {
         </div>
       </div>
       <div className='absolute bottom-0 left-0 w-full border-t md:border-t-0 dark:border-white/20 md:border-transparent md:dark:border-transparent md:bg-vert-light-gradient bg-white dark:bg-gray-800 md:!bg-transparent dark:md:bg-vert-dark-gradient'>
-        <form className='stretch mx-2 flex flex-row gap-3 pt-2 last:mb-2 md:last:mb-6 lg:mx-auto lg:max-w-3xl lg:pt-6'>
-          <div className='relative flex h-full flex-1 md:flex-col'>
-            <div className='flex ml-1 mt-1.5 md:w-full md:m-auto md:mb-2 gap-0 md:gap-2 justify-center'></div>
-            <div className='flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 relative border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]'>
-              <textarea
-                rows={1}
-                value={requestMessage}
-                onChange={(e) => setRequestMessage(e.target.value)}
-                className='m-0 w-full outline-none resize-none border-0 bg-transparent p-0 pl-2 pr-7 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:pl-0'
-              ></textarea>
-              <button
-                onClick={(e) => generateResponse(e)}
-                className='absolute p-1 rounded-md text-gray-500 bottom-1.5 right-1 md:bottom-2.5 md:right-2 hover:bg-gray-100 dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent'
-              >
-                <svg
-                  stroke='currentColor'
-                  fill='none'
-                  stroke-width='2'
-                  viewBox='0 0 24 24'
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  className='h-4 w-4 mr-1'
-                  height='1em'
-                  width='1em'
-                  xmlns='http://www.w3.org/2000/svg'
-                >
-                  <line x1='22' y1='2' x2='11' y2='13'></line>
-                  <polygon points='22 2 15 22 11 13 2 9 22 2'></polygon>
-                </svg>
-              </button>
+        {loading ? (
+          ''
+        ) : (
+          <form
+            className='stretch mx-2 flex flex-row gap-3 pt-2 last:mb-2 md:last:mb-6 lg:mx-auto lg:max-w-3xl lg:pt-6'
+            onSubmit={(e) => generateResponse(e)}
+          >
+            <div className='relative flex h-full flex-1 md:flex-col'>
+              <div className='flex ml-1 mt-1.5 md:w-full md:m-auto md:mb-2 gap-0 md:gap-2 justify-center'></div>
+              <div className='flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 relative border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]'>
+                <input
+                  type='text'
+                  value={requestMessage}
+                  onChange={(e) => setRequestMessage(e.target.value)}
+                  className='m-0 w-full outline-none resize-none border-0 bg-transparent p-0 pl-2 pr-7 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:pl-0'
+                ></input>
+                <button className='absolute p-1 rounded-md text-gray-500 bottom-1.5 right-1 md:bottom-2.5 md:right-2 hover:bg-gray-100 dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent'>
+                  <svg
+                    stroke='currentColor'
+                    fill='none'
+                    stroke-width='2'
+                    viewBox='0 0 24 24'
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                    className='h-4 w-4 mr-1'
+                    height='1em'
+                    width='1em'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <line x1='22' y1='2' x2='11' y2='13'></line>
+                    <polygon points='22 2 15 22 11 13 2 9 22 2'></polygon>
+                  </svg>
+                </button>
+              </div>
             </div>
-          </div>
-        </form>
+          </form>
+        )}
         <div className='px-3 pt-2 pb-3 text-center text-xs text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6'>
           <a
             href='https://help.openai.com/en/articles/6825453-chatgpt-release-notes'
